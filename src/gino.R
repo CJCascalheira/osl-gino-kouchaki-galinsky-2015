85734de3101df0f938fa838c2889e4c9dc989b55
Cory Cascalheira <17078341+corycaaz@users.noreply.github.com>
2018-12-10 15:25:49 -0500

    Initialize folder structure and documents

---
+++ src/gino.R
@@ -0,0 +1,10 @@
+####### SETUP WORKSPACE #######
+
+# Load dependencies
+library(tidyverse)
+
+# Set working directory
+setwd("~/GitHub/osl-gino-kouchaki-galinsky-2015/src")
+
+# Import data
+gino <- read_csv("../data/Gino Kouchaki Galinsky 2015 Experiment 3.csv")
